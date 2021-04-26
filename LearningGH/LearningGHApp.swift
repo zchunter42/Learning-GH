2e01e5e25b4a1339b47b7d9e1291faa3dd47aad5
Chase Hunter <zchunter42@gmail.com>
2021-04-26 16:26:52 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Chase Hunter on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
